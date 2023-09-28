2e6213f51a08403926246aaeab11e4210638ad55
unknown <bggth.studio@gmail.com>
2023-09-28 21:40:17 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let result = 0;
+    let d = Math.abs(n);
+    while (d != 0) {
+        o = d % 10;
+        d = (d - o) / 10;
+        result = result * 10 + o;
+    }
+    return result;
 }
